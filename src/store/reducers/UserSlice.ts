e00822918ab0672f9a8f49e4cc00031a460ea267
Sergey Otinov <otinov.prog@gmail.com>
2023-11-10 16:08:20 +0200

    fix Api_Key

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,36 +1,36 @@
-import { IUser } from "src/types/IUser"
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import IndexedDB from "src/services/db"
+import { IUser } from "src/types/IUser";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import IndexedDB from "src/services/db";
 
 interface UserState {
-	db: IndexedDB,
-	user: IUser | null,
-	isAuth: boolean,
-	error: string,
-	API_KEY: string
+    db: IndexedDB;
+    user: IUser | null;
+    isAuth: boolean;
+    error: string;
+    API_KEY: string;
 }
 
 const initialState: UserState = {
-	db: new IndexedDB('user', 1),
-	user: null,
-	isAuth: false,
-	error: '',
-	API_KEY: 'AIzaSyAtXO4WBVN1zrtPzD5rzEWwZz_f5f1Ffso'
-}
+    db: new IndexedDB("user", 1),
+    user: null,
+    isAuth: false,
+    error: "",
+    API_KEY: "your_key",
+};
 
 export const userSlice = createSlice({
-	name: 'user',
-	initialState,
-	reducers: {
-		login(state, action: PayloadAction<IUser>) {
-			state.isAuth = true;
-			state.user = action.payload;
-		},
-		logout(state) {
-			state.isAuth = false;
-			state.user = null;
-		}
-	}
-})
+    name: "user",
+    initialState,
+    reducers: {
+        login(state, action: PayloadAction<IUser>) {
+            state.isAuth = true;
+            state.user = action.payload;
+        },
+        logout(state) {
+            state.isAuth = false;
+            state.user = null;
+        },
+    },
+});
 
 export default userSlice.reducer;
